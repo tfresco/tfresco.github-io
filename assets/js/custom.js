665fb27d2600c64ca3f135c1c50e0d0410658797
Thomas Fresco <tfresco@motovan.com>
2016-12-30 10:12:53 -0500

    Back to Top + Sticky header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
         nav: true,
         items: 1
     })
+
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+
 });
