c6eebee2ae7c7454fb2d093cd18f7fdf47c64e50
Thomas Fresco <tfresco@motovan.com>
2017-01-05 10:14:00 -0500

    UI Update
    New logos
    No scrollbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
        });
        // scroll body to 0px on click
        $('#back-to-top').click(function () {
-           $('#back-to-top').tooltip('hide');
            $('body,html').animate({
                scrollTop: 0
            }, 800);
